7fe390b21afbeee156688e0f6b2679d5167ee1e4
C. Priestley <charlottempriestley@gmail.com>
2026-02-17 15:22:23 +0000

    make tle2sat_array faster by parsing TLEs directly rather than calling tle2sat_array in a loop

--- jax_sgp4/notio.py
+++ jax_sgp4/notio.py
@@ -1,6 +1,7 @@
 
 from .model import Satellite
 import jax.numpy as jnp
+import numpy as np
 
 def tle2sat(tle_1, tle_2):
 
@@ -43,6 +44,54 @@ def tle2sat(tle_1, tle_2):
         epochyr = 1900 + two_digit_year
 
     return Satellite(n0, e0, i0, w0, Omega0, M0, Bstar, epochdays, epochyr)
+
+
+def tle2sat_array_fast(tle_1_array, tle_2_array):                             
+    n = len(tle_1_array)                                                      
+
+    # Pre-allocate NumPy arrays
+    n0 = np.empty(n)
+    e0 = np.empty(n)
+    i0 = np.empty(n)
+    w0 = np.empty(n)
+    Omega0 = np.empty(n)
+    M0 = np.empty(n)
+    Bstar = np.empty(n)
+    epochdays = np.empty(n)
+    epochyr = np.empty(n)
+
+    for idx in range(n):
+        tle_1 = tle_1_array[idx]
+        tle_2 = tle_2_array[idx]
+
+        n0[idx] = float(tle_2[52:63])
+        e0[idx] = float('0.' + tle_2[26:33].replace(' ', '0'))
+        i0[idx] = float(tle_2[8:16])
+        w0[idx] = float(tle_2[34:42])
+        Omega0[idx] = float(tle_2[17:25])
+        M0[idx] = float(tle_2[43:51])
+        epochdays[idx] = float(tle_1[20:32])
+
+        bstar_mantissa = float(tle_1[53] + '.' + tle_1[54:59])
+        bexp = int(tle_1[59:61])
+        Bstar[idx] = bstar_mantissa * 10 ** bexp
+
+        two_digit_year = int(tle_1[18:20])
+        epochyr[idx] = 2000 + two_digit_year if two_digit_year < 57 else 1900 + two_digit_year
+
+    # Single bulk conversion to JAX
+    return Satellite(
+        n0=jnp.array(n0),
+        e0=jnp.array(e0),
+        i0=jnp.array(i0),
+        w0=jnp.array(w0),
+        Omega0=jnp.array(Omega0),
+        M0=jnp.array(M0),
+        Bstar=jnp.array(Bstar),
+        epochdays=jnp.array(epochdays),
+        epochyr=jnp.array(epochyr),
+    )
+
 
 # i think this needs fixing since I changed the Satellite class
 # maybe change this in the future so tle2sat can handle arrays directly?
